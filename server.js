3d3fe54b651dc1bc7cfa5217a6b5b814a966953b
Muhammadsalam Musaev <muhammadsalam@list.ru>
2024-02-05 13:13:43 +0300

    now it's working

--- server.js
+++ server.js
@@ -8,20 +8,19 @@ const server = http.createServer((request, response) => {
         case "/favicon.ico":
             console.log(request.url);
             response.writeHead(200, { "Content-Type": "image/x-icon" });
-            fs.createReadStream("/favicon.ico").pipe(response);
+            fs.createReadStream("favicon.ico").pipe(response);
+
             break;
         case "/":
         case "/students":
-            response.write("List of students");
+            response.end("List of students");
             break;
         case "/api":
-            response.write("Muslim Pro API #" + ++reqApiCount);
+            response.end("Muslim Pro API #" + ++reqApiCount);
             break;
         default:
-            response.write("404 Not Found");
+            response.end("404 Not Found");
     }
-
-    response.end();
 });
 
 server.listen(3003);
